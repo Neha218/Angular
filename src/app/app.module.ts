d2d24b3584d5f33c5b48e8d7203bf87efbae2e1a
unknown <admin@WJLP-1165.WinjitTechnologiesPvtLtd.local>
2020-11-25 16:57:10 +0530

    List Restaurant in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddRestaurantComponent } from './add-restaurant/add-restaurant.component';
+import { UpdateRestaurantComponent } from './update-restaurant/update-restaurant.component';
+import { ListRestaurantComponent } from './list-restaurant/list-restaurant.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddRestaurantComponent,
+    UpdateRestaurantComponent,
+    ListRestaurantComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot(),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
